8bc1229b1f6117d703ec4e89148eb7941f44adc6
shubhamsinha21 <shubhamsinha07084@gmail.com>
2023-05-14 05:01:11 +0530

    Learned flexbox in react-native

--- components/FlatCards.tsx
+++ components/FlatCards.tsx
@@ -1,10 +1,53 @@
 import React from "react";
-import {Text, View } from "react-native";
+import {StyleSheet, Text, View } from "react-native";
 function FlatCards() {
     return(
         <View>
-            <Text> FlatCards</Text>
+            <Text style={styles.heading}> FlatCards</Text>
+            <View style={styles.container}>
+                    {/* more tan one style can be achieved using an array */}
+                <View style={[styles.card, styles.cardOne]}>
+                    <Text>Red</Text>
+                </View>
+                <View style={[styles.card, styles.cardTwo]}>
+                    <Text>Green</Text>
+                </View>
+                <View style={[styles.card, styles.cardThree]}>
+                    <Text>Blue</Text>
+                </View>
+                
+            </View>
         </View>
     )
 }
+const styles = StyleSheet.create( {
+    heading : {
+        fontSize:24,
+        fontWeight:'bold',
+        paddingHorizontal:8
+    },
+    container:{
+        flex:1,
+        flexDirection:'row',
+        padding:8
+    },
+    card:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+        width:100,
+        height:100,
+        borderRadius:4,
+        margin:8
+    },
+    cardOne:{
+        backgroundColor:'red'
+    },
+    cardTwo:{
+        backgroundColor:'green'
+    },
+    cardThree:{
+        backgroundColor:'blue'
+    }
+})
 export default FlatCards;
