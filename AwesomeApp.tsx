b64e5eec77e4c4b1b59e7f63d0f14511ed3f6871
shubhamsinha21 <shubhamsinha07084@gmail.com>
2023-05-14 04:29:40 +0530

    New file AwesomeApp.tsx

---
+++ AwesomeApp.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { SafeAreaView, ScrollView, Text, View } from 'react-native'
+
+function AwesomeApp () {
+
+    return (
+        <SafeAreaView>
+            <ScrollView>
+             <View>
+                <Text> textInComponent </Text>
+             </View>
+            </ScrollView>
+      </SafeAreaView>
+    )
+  }
+
+
+export default AwesomeApp
--- index.js
+++ index.js
@@ -4,7 +4,8 @@
 
 import {AppRegistry} from 'react-native';
 // import App from './App';
-import AppPro from './AppPro';
+// import AppPro from './AppPro';
+import AwesomeApp from './AwesomeApp'
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => AppPro);
+AppRegistry.registerComponent(appName, () => AwesomeApp);
